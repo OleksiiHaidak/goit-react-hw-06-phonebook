4f7abe52d8532428ecef2c8a2b52462b9bfce656
OleksiiHaidak <xalexey.g@gmail.com>
2023-11-22 00:11:39 +0200

    update App and ContactsFilter components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,17 @@
-import { useSelector, useDispatch } from "react-redux";
-import { setFilter } from "redux/filter/filter.reducer";
 import css from 'components/Phonebook/Phonebook.module.css';
 import ContactForm from "components/Phonebook/ContactForm";
 import ContactList from "components/Phonebook/ContactList";
-import ContactFilter from "components/Phonebook/ContactFilter";
+import ContactsFilter from "components/Phonebook/ContactFilter";
 
 
 const App = () => {
-
-  const dispatch = useDispatch();
-  const filter = useSelector((state) => state.contactFilter);
-
-  const handleFilterChange = (evt) => {
-  const { value } = evt.target;
-  dispatch(setFilter(value));
-};
 
     return (
       <div className={css.phonebookForm}>
         <h1 className={css.mainTitle}>Phonebook</h1>
         <ContactForm/>
         <h2>Contacts</h2>
-        <ContactFilter value={filter} onChange={handleFilterChange} />
+        <ContactsFilter/>
         <ContactList/>
       </div>
     )
--- src/components/Phonebook/ContactFilter.jsx
+++ src/components/Phonebook/ContactFilter.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import css from 'components/Phonebook/Phonebook.module.css';
+import { setFilter } from "redux/filter/filter.reducer";
+import { useSelector, useDispatch } from "react-redux";
 
-const ContactFilter = ({ value, onChange }) => {
+const ContactsFilter = () => {
+
+  const dispatch = useDispatch();
+  const filter = useSelector((state) => state.contactFilter);
+
+  const handleFilterChange = (evt) => {
+  const { value } = evt.target;
+  dispatch(setFilter(value));
+};
 
   return (
     <label className={css.formLabel}>
       <p className={css.labelTitle}>Find contacts by name</p>
-      <input type="text" value={value} onChange={onChange} className={css.input} />
+      <input type="text" value={filter} onChange={handleFilterChange} className={css.input} />
     </label>
   );
 };
 
-export default ContactFilter;
+export default ContactsFilter;
